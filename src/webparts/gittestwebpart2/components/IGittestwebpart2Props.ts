f429765364a29e9af8b76384aa1f6e65eed47ee1
Manjunath Reddy <manjunathreddy1@gmail.com>
2025-07-23 20:56:38 -0500

    added spfx project

---
+++ src/webparts/gittestwebpart2/components/IGittestwebpart2Props.ts
@@ -0,0 +1,7 @@
+export interface IGittestwebpart2Props {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/gittestwebpart2/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IGittestwebpart2WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'Gittestwebpart2WebPartStrings' {
+  const strings: IGittestwebpart2WebPartStrings;
+  export = strings;
+}
